404e2a5f5e60cd9c45ed73a4cfc796fe8be6b591
Jennifer Lerma <you@example.com>
2020-02-12 09:24:06 -0600

    normalidad

---
+++ clases/script _3R.R
@@ -0,0 +1,16 @@
+
+
+
+
+#Normalidad -------------------------------------------
+
+head (conjunto)
+
+# Aplicar prueba de shapiro. test para la variable Altura 
+
+shapiro.test(conjunto$Altura)
+
+ks.test(conjunto$diametro), ´´pnorm´´, mean=mean(conujunto$diametro
+                                                 sd=sd(conjunto$diametro))
+
+hist(conjunto$diametro)
